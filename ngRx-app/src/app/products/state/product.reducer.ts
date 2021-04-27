d7088fbf37d8de3f6468ff398533749ee849ce0f
catgsmith <catgsmith@hotmail.com>
2021-04-27 12:31:52 +0100

    create feature sepectors for product

--- ngRx-app/src/app/products/state/product.reducer.ts
+++ ngRx-app/src/app/products/state/product.reducer.ts
@@ -1,4 +1,4 @@
-import { createAction, createReducer, on } from '@ngrx/store';
+import { createAction, createFeatureSelector, createReducer, createSelector, on } from '@ngrx/store';
 import * as AppState from '../../state/app.state';
 
 import { Product } from '../product';
@@ -13,8 +13,31 @@ export interface ProductState {
     products: Product[];
 }
 
+const initialState: ProductState = {
+    showProductCode: true,
+    currentProduct: null,
+    products: []
+}
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+    getProductFeatureState,
+    state => state.showProductCode
+);
+
+export const getCurrentProduct = createSelector(
+    getProductFeatureState,
+    state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+    getProductFeatureState,
+    state => state.products
+);
+
 export const productReducer = createReducer<ProductState>(
-    { showProductCode: true } as ProductState,
+    initialState,
     on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
         
         return {
