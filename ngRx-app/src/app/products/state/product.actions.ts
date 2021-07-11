9fc36b71164fb9192a613169467069b47d70e65c
Catherine Gaughan-Smith <catgsmith@hotmail.com>
2021-07-11 19:50:51 +0100

    add UpdateProducts actions and effect

--- ngRx-app/src/app/products/state/product.actions.ts
+++ ngRx-app/src/app/products/state/product.actions.ts
@@ -31,3 +31,19 @@ export const loadProductsFailure = createAction(
     '[Product] Load Fail',
     props<{ error: string }>()
 );
+
+export const updateProduct = createAction(
+    '[Product] Update Product',
+    props<{ product: Product }>()
+);
+
+export const updateProductSuccess = createAction(
+    '[Product] Update Product Success',
+    props<{ product: Product }>() // this is the product returned from the update operation
+);
+
+export const updateProductFailure = createAction(
+    '[Product] Update Product Fail',
+    props<{ error: string }>()
+);
+
